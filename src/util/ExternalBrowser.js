8ec52f91ad4c1924a0832c1f7b31046d7ebbaf8f
Ariel Kirkwood <ariel.kirkwood@me.com>
2015-01-04 17:04:00 -0500

    Removes whitespace

--- src/util/ExternalBrowser.js
+++ src/util/ExternalBrowser.js
@@ -3,7 +3,7 @@
  */
 Ext.define('Jarvus.util.ExternalBrowser', {
     singleton: true,
-    
+
     /**
      * Open a link in a new tab/window via javascript
      */
@@ -14,14 +14,14 @@ Ext.define('Jarvus.util.ExternalBrowser', {
         }
         var anchorNode = document.createElement('a'),
             fakeEvent = document.createEvent("HTMLEvents");
-            
+
         fakeEvent.initEvent('click', true, true);
-            
+
         anchorNode.setAttribute('href', url);
         anchorNode.setAttribute('target', '_blank');
         anchorNode.dispatchEvent(fakeEvent);
     },
-    
+
     /**
      * Finds any <a> tags within the passed DOM element and ensures their target="_blank" if
      * the href attribute doesn't start with #
@@ -29,15 +29,15 @@ Ext.define('Jarvus.util.ExternalBrowser', {
     patchTargets: function(dom) {
         dom = Ext.getDom(dom);
         var links, i = 0, linkNode;
-        
+
         links = dom.querySelectorAll('a:not([href^="#"]):not([target=_blank])');
         for (; linkNode = links[i]; i++) {
             linkNode.target = '_blank';
         }
-        
+
         return dom;
     },
-    
+
     /**
      * Sets up a handler for external links on environments where special handling is needed to
      * launch links into external browser windows (specifically, using ChildBrowser on Cordova/PhoneGap)
@@ -47,7 +47,7 @@ Ext.define('Jarvus.util.ExternalBrowser', {
             // must attach DOM event listener directly as Sencha Touch 2.2 removes support for listening to real DOM events
             document.body.addEventListener('click', function(e) {
                 var target = Ext.fly(e.target).findParent('a[target=_blank],a[target=_system]');
-                
+
                 if (target) {
                     e.preventDefault();
                     window.open(target.href, target.target, 'location=no,enableViewportScale=yes,toolbarposition=top');
@@ -55,7 +55,7 @@ Ext.define('Jarvus.util.ExternalBrowser', {
             }, false);
         }
     },
-    
+
     // @private
     isHomescreenApp: function() {
         return ('standalone' in navigator) && navigator.standalone;
